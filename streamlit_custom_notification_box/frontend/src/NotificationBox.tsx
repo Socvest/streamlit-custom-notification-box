ea0dc7864e6c0aa4107d64cd306bda0322049d30
Socvest <82335390+Socvest@users.noreply.github.com>
2022-05-28 06:09:01 +0100

    Update NotificationBox.tsx

--- streamlit_custom_notification_box/frontend/src/NotificationBox.tsx
+++ streamlit_custom_notification_box/frontend/src/NotificationBox.tsx
@@ -38,18 +38,7 @@ class NotificationBox extends StreamlitComponentBase {
           <a className="link" href={url} {...style(styles['link'])}>{externalLink}</a>
           <div className="close-button" onClick={removeNotification} {...style(styles['close-button'])}>x</div>
         </div>
-      </div>
-      
-      // <div className="notification-component">
-      //   <span className="container-for-text-icon" {...style(styles['container-for-text-icon'])}>
-      //    <div className="icon-container" {...style(styles['icon-container'])}>
-      //        <i className="material-icons" {...style(styles['material-icons'])}>{icon}</i></div>
-      //          <div className="text-info-display" {...style(styles['text-info-display'])}>{textDisplay}</div></span>
-      //          <span> </span>
-      //          <a className="external-info" href={url} {...style(styles['external-info'])}>{externalLink}</a>
-      //          <div className="close-display" onClick={removeNotification}>X</div>
-      //      </div>   
-    
+      </div>   
 
           )
     }
