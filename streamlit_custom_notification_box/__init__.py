4caf16c6d3f79303ba0ace38c6c8e0b691049268
Socvest <82335390+Socvest@users.noreply.github.com>
2022-05-29 17:10:22 +0100

    Update __init__.py

--- streamlit_custom_notification_box/__init__.py
+++ streamlit_custom_notification_box/__init__.py
@@ -5,9 +5,9 @@ import streamlit.components.v1 as components
 _RELEASE = True
 
 if not _RELEASE:
-    _streamlit_custom_notification_box = components.declare_component(
+    _custom_notification_box = components.declare_component(
         
-        "streamlit_custom_notification_box",
+        "custom_notification_box",
 
         url="http://localhost:3001",
     )
@@ -15,12 +15,12 @@ else:
 
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
-    _streamlit_custom_notification_box = components.declare_component("streamlit_custom_notification_box", path=build_dir)
+    _streamlit_custom_notification_box = components.declare_component("custom_notification_box", path=build_dir)
 
 
-def streamlit_custom_notification_box(icon, textDisplay, externalLink, url, styles, key=None, defaultIndex=0):
+def custom_notification_box(icon, textDisplay, externalLink, url, styles, key=None, defaultIndex=0):
 
-    return _streamlit_custom_notification_box(icon=icon, 
+    return _custom_notification_box(icon=icon, 
                                             textDisplay=textDisplay, 
                                             externalLink=externalLink, 
                                             url=url, 
